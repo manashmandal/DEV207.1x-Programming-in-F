4312100ccff134908515f99ff4028ab8f1b60409
Manash Mandal <manashmndl@gmail.com>
2015-10-02 21:35:07 +0600

    Module1_Koan Problem

---
+++ Module1Koan/Module1Koan/AssemblyInfo.fs
@@ -0,0 +1,41 @@
+﻿namespace Module1Koan.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[<assembly: AssemblyTitle("Module1Koan")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("Module1Koan")>]
+[<assembly: AssemblyCopyright("Copyright ©  2015")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[<assembly: ComVisible(false)>]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[<assembly: Guid("d3f37b48-9bb2-406a-b785-1a55836d8cb0")>]
+
+// Version information for an assembly consists of the following four values:
+// 
+//       Major Version
+//       Minor Version 
+//       Build Number
+//       Revision
+// 
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [<assembly: AssemblyVersion("1.0.*")>]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+do
+    ()
---
+++ Module1Koan/Module1Koan/Program.fs
@@ -0,0 +1,17 @@
+﻿//Immutable problem was solved
+
+open System
+
+let a = 100
+let b = 1000
+
+
+let f x y = x + y / 10
+
+[<EntryPoint>]
+let main argv = 
+    let a = 101
+    let z = f a b
+    printfn "%A" z
+    Console.ReadKey()
+    0 // return an integer exit code
