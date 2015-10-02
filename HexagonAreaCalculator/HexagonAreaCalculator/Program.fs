933c85e8545ec31b3443b3981f90e216130b3a67
Manash Mandal <manashmndl@gmail.com>
2015-10-02 21:25:20 +0600

    module 1 assesment 2

---
+++ HexagonAreaCalculator/HexagonAreaCalculator/Program.fs
@@ -0,0 +1,18 @@
+﻿//Calculates area of a hexagon
+
+open System
+let hexarea t:float = (3.0 * Math.Sqrt(3.0) / 2.0) * Math.Pow(t, 2.0)
+
+
+[<EntryPoint>]
+let main argv = 
+    let value = Console.ReadLine()
+    let f = float value
+
+    //Execute Calc
+    let calc = hexarea f
+
+    printf "%f" calc
+
+    Console.ReadKey()
+    0 // return an integer exit code
