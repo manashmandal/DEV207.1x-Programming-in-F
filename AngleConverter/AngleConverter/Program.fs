dc9c374e24acc8e5105b8c04d4e671fb164bf405
Manash Mandal <manashmndl@gmail.com>
2015-10-02 21:18:20 +0600

    new files

---
+++ AngleConverter/AngleConverter/Program.fs
@@ -0,0 +1,17 @@
+﻿//A F# Program for angle conversion [degree <-> radian]
+
+open System
+
+//F# Hands on assesment 1
+//Convert 67 in radian
+
+let deg2rad (deg:float) = (deg * 3.14) / 180.0
+let rad2deg (rad:float) = (rad * 180.0) / 3.14
+
+
+[<EntryPoint>]
+let main argv = 
+    let radian = deg2rad 67.0
+    Console.Write("67.0 Degree in radian: {0}", radian)
+    Console.ReadKey()
+    0 // return an integer exit code
