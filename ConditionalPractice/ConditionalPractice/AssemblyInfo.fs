23d9731a6337fa232ed4676ead725d78558b3220
Manash Mandal <manashmndl@gmail.com>
2015-10-08 01:47:10 +0600

    Conditional in F#

---
+++ ConditionalPractice/ConditionalPractice/AssemblyInfo.fs
@@ -0,0 +1,41 @@
+﻿namespace ConditionalPractice.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[<assembly: AssemblyTitle("ConditionalPractice")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("ConditionalPractice")>]
+[<assembly: AssemblyCopyright("Copyright ©  2015")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[<assembly: ComVisible(false)>]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[<assembly: Guid("86d20b01-5376-4b02-8d19-41ba3c210b68")>]
+
+// Version information for an assembly consists of the following four values:
+// 
+//       Major Version
+//       Minor Version 
+//       Build Number
+//       Revision
+// 
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [<assembly: AssemblyVersion("1.0.*")>]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+do
+    ()
---
+++ ConditionalPractice/ConditionalPractice/Program.fs
@@ -0,0 +1,21 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+
+open System
+
+let checknum v = 
+    if v = 5 then
+        "I am 5"
+    elif v < 5 then
+        "I am less than 5"
+    else 
+        "I am another number"
+     
+
+[<EntryPoint>]
+let main argv = 
+    let check = Console.ReadLine()
+    let check = int check
+    printfn "%A" (checknum check)
+    Console.ReadKey();
+    0
